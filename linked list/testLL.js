5db1019363a5ad2d4992b18a20d35ea2e9821905
elgeokareem <elkareem123@gmail.com>
2023-06-18 12:34:14 -0400

    redone again push pop unshift

--- linked list/testLL.js
+++ linked list/testLL.js
@@ -7,25 +7,29 @@ class Node {
 
 class LinkedList {
   constructor(value) {
+    if (value) {
+      const startNode = new Node(value);
+
+      this.head = startNode;
+      this.tail = startNode;
+      this.length = 1;
+      return;
+    }
+
     this.head = null;
     this.tail = null;
     this.length = 0;
-
-    if (value) {
-      const newNode = new Node(value);
-      this.head = newNode;
-      this.tail = newNode;
-      this.length = 1;
-    }
   }
 
+  // Empuja un nuevo nodo a la lista
   push(value) {
     const newNode = new Node(value);
 
     if (this.length === 0) {
       this.head = newNode;
-      this.tail = newNode;
+      this.head = newNode;
       this.length = 1;
+
       return this;
     }
 
@@ -36,13 +40,17 @@ class LinkedList {
     return this;
   }
 
+  // Saca el ultimo nodo de la lista
   pop() {
+    // tenemos que crear 2 variables para guardar la referencia
+    // esto es debido a que para llegar al ultimo nodo tenemos que recorrer toda la lista
     if (this.length === 0) {
-      return undefined;
+      return;
     }
 
     let pre = this.head;
     let temp = this.head.next;
+
     while (temp.next !== null) {
       pre = temp;
       temp = temp.next;
@@ -55,6 +63,7 @@ class LinkedList {
     return temp;
   }
 
+  // agregar un item de primero
   unshift(value) {
     const newNode = new Node(value);
 
@@ -62,23 +71,22 @@ class LinkedList {
       this.head = newNode;
       this.tail = newNode;
       this.length = 1;
+
       return this;
     }
 
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
+
     return this;
   }
-
-  // shift
 }
 
-const ll = new LinkedList();
-ll.push(5);
-ll.push(6);
-ll.push(7);
-ll.unshift(4);
+const ll = new LinkedList(1);
+ll.push(2);
+ll.push(3);
 ll.pop();
+ll.unshift(0);
 
 console.log(JSON.stringify(ll, null, 2));
