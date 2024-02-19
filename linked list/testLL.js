aa343de4d8c7942ec28c7a7d15977f4439e653b7
William <elkareem123@gmail.com>
2024-02-18 23:28:41 -0400

    redone methods

--- linked list/testLL.js
+++ linked list/testLL.js
@@ -7,28 +7,27 @@ class Node {
 
 class LinkedList {
   constructor(value) {
-    if (value) {
-      const startNode = new Node(value);
+    if (!value) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
 
-      this.head = startNode;
-      this.tail = startNode;
-      this.length = 1;
       return;
     }
 
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
   }
 
-  // Empuja un nuevo nodo a la lista
   push(value) {
     const newNode = new Node(value);
 
-    if (this.length === 0) {
+    if (!this.length) {
       this.head = newNode;
-      this.head = newNode;
-      this.length = 1;
+      this.tail = newNode;
+      this.length++;
 
       return this;
     }
@@ -40,37 +39,43 @@ class LinkedList {
     return this;
   }
 
-  // Saca el ultimo nodo de la lista
+  // Remove last element from LL
   pop() {
-    // tenemos que crear 2 variables para guardar la referencia
-    // esto es debido a que para llegar al ultimo nodo tenemos que recorrer toda la lista
     if (this.length === 0) {
       return;
+    }
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+
+      return this.head; // or tail;
     }
 
     let pre = this.head;
     let temp = this.head.next;
 
     while (temp.next !== null) {
-      pre = temp;
+      pre = pre.next;
       temp = temp.next;
     }
 
-    pre.next = null;
     this.tail = pre;
+    this.tail.next = null;
     this.length--;
 
     return temp;
   }
 
-  // agregar un item de primero
+  // Add element to start of the LL
   unshift(value) {
     const newNode = new Node(value);
 
-    if (this.length === 0) {
+    if (!this.length) {
       this.head = newNode;
       this.tail = newNode;
-      this.length = 1;
+      this.length++;
 
       return this;
     }
@@ -81,12 +86,60 @@ class LinkedList {
 
     return this;
   }
+
+  // Remove first element
+  shift() {
+    if (this.length === 0) {
+      return;
+    }
+
+    if (this.length === 1) {
+      const removedItem = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+
+      return removedItem; // or tail
+    }
+
+    const removedNode = this.head;
+    this.head = this.head.next;
+
+    return removedNode;
+  }
+
+  // get node from index
+  get(index) {
+    if (index >= this.length) {
+      throw new Error("ayy lmao");
+    }
+
+    let i = 0;
+    let node = this.head;
+    while (index < this.length && i !== index) {
+      node = node.next;
+      i++;
+    }
+
+    return node;
+  }
+
+  // Setting value given an index
+  set(index, value) {
+    const node = this.get(index);
+    node.value = value;
+
+    return this;
+  }
 }
 
-const ll = new LinkedList(1);
-ll.push(2);
-ll.push(3);
-ll.pop();
-ll.unshift(0);
+const ll1 = new LinkedList();
 
-console.log(JSON.stringify(ll, null, 2));
+ll1.push(1);
+ll1.push(2);
+ll1.push(3);
+ll1.push(4);
+ll1.push(5);
+ll1.set(4, 100);
+
+console.log(JSON.stringify(ll1, null, 2));
