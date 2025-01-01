a6a4029b4c4f6e24b4583ba31ef47ff544005289
William <elkareem123@gmail.com>
2025-01-01 12:04:59 -0400

    studying again

--- linked list/1-testing.js
+++ linked list/1-testing.js
@@ -5,4 +5,64 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  // head siempre va de primero (izquierda)
+  constructor(value) {
+    if (value) {
+      this.length = 1;
+      this.head = new Node(value);
+      this.tail = new Node(value);
+      return;
+    }
+
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  print() {
+    console.log(JSON.stringify(this, null, 2));
+  }
+
+  push(value) {
+    const node = new Node(value);
+
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+      return;
+    }
+
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+  }
+
+  pop() {
+    if (this.length === 0) return;
+    if (this.length === 1) {
+      this.length--;
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+
+    let temp = this.head;
+    for (let index = 1; index < this.length - 1; index++) {
+      temp = temp.next;
+    }
+
+    temp.next = null;
+    this.tail = temp;
+    this.length--;
+  }
+}
+
+const ll = new LinkedList();
+
+ll.push(1);
+ll.push(2);
+ll.push(3);
+
+ll.print();
